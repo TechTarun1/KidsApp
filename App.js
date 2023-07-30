199ef981211081e89c03160006e828f3c9c8c3b9
Tarun <Tarun.Boddeboyina@wallero.com>
2023-07-30 11:08:40 +0530

    done with local api pointing

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { StatusBar } from 'react-native';
 import { StyleSheet, SafeAreaView } from 'react-native';
 import BottomNavigation from './navigation/BottomNavigation';
@@ -5,6 +6,8 @@ import Congrats from './screens/Congrats';
 import Alphabet from './screens/Alphabet';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import axios from 'axios';
+
 import Level2 from './screens/Level2';
 import Level3 from './screens/Level3';
 import Level5 from './screens/Level5';
@@ -13,6 +16,12 @@ import Level4 from './screens/Level4';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  useEffect(() => {
+    axios.get('https://few-boxes-pay.loca.lt/').then((res) => {
+      console.log('res', res.data);
+    })
+  })
 
   const statusbarHeight = StatusBar.currentHeight | 0;
 
